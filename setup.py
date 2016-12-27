372e6431e79199ad6e0b194a22222a3171645b26
Marten Lietz <marten.lietz@adup-tech.com>
2016-12-27 17:44:58 +0100

    remove .gitmodules

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
 
-setup(name='MFRC522-python',
-      version='1.0',
-      description='A small class to interface with the NFC reader Module MFRC522 on the Raspberry Pi',
-      author='mxgxw',
-      author_email='mxgxw@example.com',
-      packages=['MFRC522'],
-      dependency_links=['https://github.com/lthiery/SPI-Py.git@master']
-     )
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='MFRC522-python',
+    version='0.0.1',
+    description='A small class to interface with the NFC reader Module MFRC522 on the Raspberry Pi',
+    author='mxgxw',
+    author_email='mxgxw@example.com',
+    packages=['MFRC522'],
+    dependency_links=['https://github.com/lthiery/SPI-Py.git@master'],
+    long_description=read('README.md')
+)
