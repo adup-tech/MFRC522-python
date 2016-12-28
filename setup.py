8f67002f27f8828d7bf90d2dbd85163dcc17b282
Marten Lietz <marten.lietz@adup-tech.com>
2016-12-28 10:33:14 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     name='MFRC522-python',
     version='0.0.1',
     description='A small class to interface with the NFC reader Module MFRC522 on the Raspberry Pi',
-    dependency_links=['https://github.com/lthiery/SPI-Py.git@master#egg=SPI-Py'],
-    long_description=read('README.md')
+    long_description=read('README.md'),
+    py_modules=['MFRC522']
 )
